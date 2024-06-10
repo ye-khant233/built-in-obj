4b9b5cd3096b2320d9cbd2c790aa93fb39e7d3a4
YeKhant <yekhant791@gmail.com>
2024-06-10 17:29:30 +0530

    add objs

---
+++ builtinObj.js
@@ -0,0 +1,65 @@
+// console.dir(Date);
+// // console.log(Date.now());
+// // console.log(Date.parse());
+// const date = new Date();
+// const monthNames = [
+//     "January", "February", "March", "April", "May", "June",
+//     "July", "August", "September", "October", "November", "December"
+//   ];
+//   const dayNames = [
+//     "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+//   ];
+  
+// console.log(date);
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toLocaleDateString());
+
+// console.log(date.getFullYear());
+// console.log(monthNames[date.getMonth()]);
+// console.log(dayNames[date.getDate()]);
+
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+
+
+// console.log(date.getTime()
+
+
+
+// console.log(Date);
+
+// const d = new Date(9999999);
+// console.log(d);
+
+
+// const d = new Date();
+
+// // d.setHours(d.getHours()+300);
+
+// // d.setMonth(d.getMonth() + 4)
+
+
+// const totalSwdCourseHours = 50;
+// const totalLearningHours = 2;
+// const learningDay = totalSwdCourseHours / totalLearningHours;
+// d.setDate(d.getDate()+ learningDay);
+// console.log(d);
+
+
+
+
+// 2024.5.22 nuk yak 100 gka bl nae ll thi chin dh 
+
+
+// const date = new Date(2024,4,22);
+// date.setDate(date.getDate() + 100)
+
+// console.log(date);
+
+
+console.dir(Number);
+
+
