525c478479ff2f37e54fca155a169f367fe48d33
YeKhant <yekhant791@gmail.com>
2024-06-12 02:08:25 +0530

    modified array

---
+++ arraytype.js
@@ -0,0 +1,52 @@
+// console.dir(Array);;
+// const nums = [1,2,3,4];
+// const char = ["a","c","d","e"]
+// const emojis = ["0","2"]
+
+// // const results = nums.concat(char).concat(emojis)
+
+// const results = [...nums,...char,...emojis]
+
+// console.log(results);
+
+
+// const arr = []
+// arr.push("aaa")
+// arr.push("bb")
+// arr.push("cc")
+// console.log(arr);
+
+const nums = [1,3,2,5]
+nums.pop()
+nums.pop()
+nums.pop()
+console.log(nums);
+// const fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape", "Honeydew", "Kiwi", "Lemon"];
+
+// console.log(fruits.length);
+// console.log(fruits.toString());
+// console.log(fruits.join("\n"));
+// console.log(fruits.sort());
+
+// console.log(fruits);
+
+
+
+// const array = ["one","two","three","five"]
+
+// array.reverse();
+// console.log(array);
+
+
+
+// console.log(Array.isArray(fruits));
+
+// console.log(fruits.includes("Apple"));
+// console.log(fruits.includes("apple"));
+// console.log(fruits.indexOf("Apple"));
+
+
+// const parts = fruits.slice(0,4);
+// console.log(parts);
+
+// console.log(fruits);
